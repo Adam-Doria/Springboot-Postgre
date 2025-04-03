030da53ac6944da1c5eb84398cb3c6ebf5a7eef2
Adam-Doria <drici.adam@gmail.com>
2025-04-03 13:49:10 +0200

    feat : update socket for private message and add jwt token to secure connexion

--- src/main/java/RealTimeChat/repository/PrivateConversationRepository.java
+++ src/main/java/RealTimeChat/repository/PrivateConversationRepository.java
@@ -3,6 +3,8 @@ package RealTimeChat.repository;
 import RealTimeChat.model.PrivateConversation;
 import RealTimeChat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,4 +16,10 @@ public interface PrivateConversationRepository extends JpaRepository<PrivateConv
     Optional<PrivateConversation> findByUser1AndUser2(User user1, User user2);
 
     List<PrivateConversation> findByUser1OrUser2(User user1, User user2);
+
+    // Si on se chauffe avec les notifications c'est bon de savoir combien y'a de message non lus
+    @Query("SELECT COUNT(m) FROM Message m WHERE m.privateConversation IN " +
+            "(SELECT pc FROM PrivateConversation pc WHERE pc.user1 = :user OR pc.user2 = :user) " +
+            "AND m.recipient = :user AND m.isRead = false")
+    long countUnreadMessagesByUser(@Param("user") User user);
 }
